c9e5890332e98975a8686cdaded97fa78a5e389e
Joaquin Silva <joaquin.silva.dd@gmail.com>
2022-05-05 21:41:21 -0400

    se agrego la calculadora

--- js/contacto.js
+++ js/contacto.js
@@ -37,11 +37,11 @@ $(document).ready(function(){
         }
 
         if(errores == '' == false){
-            var mensaje='<div class="ventanaError"><div class="mensajeError">'+errores+'<span id=btnClose>Cerrar</span></div></div>'
+            var mensaje='<div class="ventana"><div class="mensaje">'+errores+'<span id=btnClose>Cerrar</span></div></div>'
             $('body').append(mensaje);
         } else{
             $('#btnClose').click(function(){
-                $('.ventanaError').remove();
+                $('.ventana').remove();
             })
             $('formulario').submit(function(evento){
                 evento.preventDefault();
@@ -56,7 +56,66 @@ $(document).ready(function(){
         }
 
         $('#btnClose').click(function(){
-            $('.ventanaError').remove();
+            $('.ventana').remove();
+        })
+    })
+
+    $('.btn-imc').click(function(){
+        var errores='';
+
+        if($("peso").val() == ''){
+            errores += '<p>Escriba su peso</p>';
+            //$('#nombre').css("border","#F14B4B")
+            $('#peso').css("border-bottom-color","#F14B4B")
+        }else{
+            //$('#nombre').hide("border-bottom-color","#F14B4B")
+            $('#peso').css("border-bottom-color","#999")
+        }
+
+        if($("altura").val() == ''){
+            errores += '<p>Ingrese su altura</p>';
+            $('#altura').css("border-bottom-color","#F14B4B")
+        } else{
+            //$('#nombre').hide("border-bottom-color","#F14B4B")
+            $('#altura').css("border-bottom-color","#999")
+        }
+
+        if(errores == '' == false){
+            var mensaje='<div class="ventana"><div class="mensaje">'+errores+'<span id=btnClose>Cerrar</span></div></div>'
+            $('body').append(mensaje);
+        } else{
+            $('#btnClose').click(function(){
+                $('.ventana').remove();
+            })
+            $('formulario').submit(function(evento){
+                evento.preventDefault();
+                var peso = $('#peso').val();
+                var altura = ($('#altura').val())/100;
+
+                var imc=peso/(altura*altura);
+                
+                errores += '<p>Su IMC es de :'+imc+'</p>';
+
+                if(imc<18.5){
+                    errores += '<p>Usted tiene bajo peso, se le recomienda que aumente su masa</p>';
+                } else{
+                    if(imc<24.9){
+                        errores += '<p>Usted tiene un buen peso, se le recomienda que lo mantenga</p>';
+                    } else{
+                        if(imc<29.9){
+                            errores += '<p>Usted tiene sobre peso, se le recomienda que baje su masa</p>';
+                        }
+                    }
+                }                
+                
+                var mensaje='<div class="ventana"><div class="mensaje">'+errores+'<><span id=btnClose>Cerrar</span></div></div>'
+                $('body').append(mensaje);
+                
+            })
+        }
+
+        $('#btnClose').click(function(){
+            $('.ventana').remove();
         })
     })
     
