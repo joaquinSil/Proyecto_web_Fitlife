903c5b23a1f417158fee74658106874df2bd50e8
Andres Vidal <andres.legue@gmail.com>
2022-05-08 17:07:11 -0400

    Videos modificados

--- js/Ejercicio.js
+++ js/Ejercicio.js
@@ -1,4 +1,3 @@
-
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     console.log("xddd");
@@ -8,8 +7,9 @@
         console.log("xddddd");
     }
     window.addEventListener("load", xd);
+    let listaVideosDestacados = Array();
     let listaVideos = Array();
-    listaVideos = [
+    listaVideosDestacados = [
         {
             "nombre": "Rutina de abdomen",
             "link": "https://drive.google.com/file/d/1pWN8cGyarrqfL06FXxA5i8p74_lkImll/preview",
@@ -29,15 +29,65 @@
             "id": 3,
         }
     ];
-    function ListarVideos() {
+    listaVideos = [
+        {
+            "nombre": "Rutina de abdomen",
+            "link": "https://drive.google.com/file/d/1pWN8cGyarrqfL06FXxA5i8p74_lkImll/preview",
+            "ranking": 3,
+            "id": 1,
+        },
+        {
+            "nombre": "Rutina de piernas",
+            "link": "https://www.youtube.com/embed/nKmH2Laeirk",
+            "ranking": 3,
+            "id": 2,
+        },
+        {
+            "nombre": "Rutina de brazos",
+            "link": "https://www.youtube.com/embed/HFm4mVhXCFI",
+            "ranking": 3,
+            "id": 3,
+        },
+        {
+            "nombre": "Rutina HITT 10 min",
+            "link": "https://www.youtube.com/embed/3A_27w5CR6A",
+            "ranking": 3,
+            "id": 4,
+        },
+        {
+            "nombre": "Rutina HITT 20 min",
+            "link": "https://drive.google.com/file/d/1VzQ432GzFU1xBIeqpqEEoo-i1MeGUl0a/preview",
+            "ranking": 3,
+            "id": 5,
+        },
+        {
+            "nombre": "Rutina HITT 30 min",
+            "link": "https://drive.google.com/file/d/12zal_N-672gMhi4sjjSP2x_d2Vy-KFqm/preview",
+            "ranking": 3,
+            "id": 6,
+        }
+    ];
+    function ListarVideosDestacados() {
         let i;
         let listaDeVideos = document.getElementById("ejerciciosDestacados");
+        let li = document.createElement("li");
+        for (i = 0; i < listaVideosDestacados.length; i++) {
+            let li = document.createElement("li");
+            listaDeVideos.appendChild(li).innerHTML = `<iframe width="320" height="240" src =" ${listaVideosDestacados[i].link}">`;
+        }
+    }
+    function ListarVideos() {
+        let i;
+        let listaDeVideos = document.getElementById("Todas las rutinas");
         let li = document.createElement("li");
         for (i = 0; i < listaVideos.length; i++) {
             let li = document.createElement("li");
             listaDeVideos.appendChild(li).innerHTML = `<iframe width="320" height="240" src =" ${listaVideos[i].link}">`;
+            if (i == 2) {
+                let p = document.createElement("br");
+            }
         }
     }
     window.addEventListener("load", xd);
+    window.addEventListener("load", ListarVideosDestacados);
     window.addEventListener("load", ListarVideos);
-
--- ts/Ejercicio.ts
+++ ts/Ejercicio.ts
@@ -10,9 +10,10 @@ function xd()
 window.addEventListener("load" ,xd);
 import {video} from './Videos';
 
+let listaVideosDestacados = Array <video>();
 let listaVideos = Array <video>();
 
-listaVideos =[
+listaVideosDestacados =[
     {
         "nombre":"Rutina de abdomen",
         "link":"https://drive.google.com/file/d/1pWN8cGyarrqfL06FXxA5i8p74_lkImll/preview",
@@ -32,15 +33,70 @@ listaVideos =[
         "id":3,
     }   
 ];
-function ListarVideos(){
+
+listaVideos =[
+    {
+        "nombre":"Rutina de abdomen",
+        "link":"https://drive.google.com/file/d/1pWN8cGyarrqfL06FXxA5i8p74_lkImll/preview",
+        "ranking":3,
+        "id":1,
+    },    
+    {
+        "nombre":"Rutina de piernas",
+        "link":"https://www.youtube.com/embed/nKmH2Laeirk",
+        "ranking":3,
+        "id":2,
+    },   
+    {
+        "nombre":"Rutina de brazos",
+        "link":"https://www.youtube.com/embed/HFm4mVhXCFI" ,
+        "ranking":3,
+        "id":3,
+    },  
+    {
+        "nombre":"Rutina HITT 10 min",
+        "link":"https://www.youtube.com/embed/3A_27w5CR6A" ,
+        "ranking":3,
+        "id":4,
+    },    
+    {
+        "nombre":"Rutina HITT 20 min",
+        "link":"https://drive.google.com/file/d/1VzQ432GzFU1xBIeqpqEEoo-i1MeGUl0a/preview",
+        "ranking":3,
+        "id":5,
+    },   
+    {
+        "nombre":"Rutina HITT 30 min",
+        "link":"https://drive.google.com/file/d/12zal_N-672gMhi4sjjSP2x_d2Vy-KFqm/preview"  ,
+        "ranking":3,
+        "id":6,
+    }    
+];
+function ListarVideosDestacados(){//aun no esta listo la funcinalidad de videos destacados
     let i:number;
     let listaDeVideos:any = document.getElementById("ejerciciosDestacados");
+    let li:any=document.createElement("li");
+    for(i=0;i<listaVideosDestacados.length;i++)
+    {
+        let li:any=document.createElement("li");
+        listaDeVideos.appendChild(li).innerHTML=`<iframe width="320" height="240" src =" ${listaVideosDestacados[i].link}">` ;
+    }
+}
+function ListarVideos(){//aun no esta listo la funcinalidad de videos destacados
+    let i:number;
+    let listaDeVideos:any = document.getElementById("Todas las rutinas");
     let li:any=document.createElement("li");
     for(i=0;i<listaVideos.length;i++)
     {
         let li:any=document.createElement("li");
         listaDeVideos.appendChild(li).innerHTML=`<iframe width="320" height="240" src =" ${listaVideos[i].link}">` ;
+        if(i==2)
+        {
+            let p:any=document.createElement("p");
+        }
     }
 }
 window.addEventListener("load" ,xd);
+
+window.addEventListener("load" ,ListarVideosDestacados);
 window.addEventListener("load" ,ListarVideos);
